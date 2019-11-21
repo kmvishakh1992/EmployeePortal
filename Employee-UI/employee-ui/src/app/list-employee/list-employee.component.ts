c21a769456ae11e48cf18ab1de04037580fb63f0
Vishakh <kmsree1990@gmail.com>
2019-11-21 11:24:56 +0530

    dummy values

--- Employee-UI/employee-ui/src/app/list-employee/list-employee.component.ts
+++ Employee-UI/employee-ui/src/app/list-employee/list-employee.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListEmployeeComponent implements OnInit {
 
+  employeeData=[
+    {
+      firstName: 'test1',
+      lastName: 'lname1',
+      gender: 'male',
+      dob: 'test',
+      department: 'dept1'
+    },
+    {
+      firstName: 'test2',
+      lastName: 'lname1',
+      gender: 'male',
+      dob: 'test',
+      department: 'dept1'
+    },
+    {
+      firstName: 'test3',
+      lastName: 'lname1',
+      gender: 'male',
+      dob: 'test',
+      department: 'dept1'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
