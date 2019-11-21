62bbd583642c3b6022d4dcaa87ded20f9b02f148
Vishakh <kmsree1990@gmail.com>
2019-11-21 09:12:51 +0530

    initial commit

---
+++ Employee-API/employee-rest-web-services/src/main/java/com/employee/employeerestwebservices/EmployeeRestWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.employee.employeerestwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRestWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRestWebServicesApplication.class, args);
+	}
+
+}
---
+++ Employee-API/employee-rest-web-services/src/test/java/com/employee/employeerestwebservices/EmployeeRestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.employeerestwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
